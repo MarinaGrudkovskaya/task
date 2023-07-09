a0aa995a9c62da0d08aed4d248e207bfdc817a6c
MarinaGrudkovskaya <mgg1005@yandex.ru>
2023-07-09 11:03:43 +0300

    Закоммитила задачу счет-нечет. Долго делала, много ошибок по синтаксису

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и 
+// выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.WriteLine("Напишите числа:");
+
+// int a = int.Parse(Console.ReadLine());
+// int b = int.Parse(Console.ReadLine());
+//  if (b > a)
+//  {
+//     Console.WriteLine("max = " + b);
+//  } 
+// else
+// {
+//   Console.WriteLine("max = " + a);  
+// }
+
+// Задача 4: Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine("Введите три числа:");
+
+// int x = int.Parse(Console.ReadLine());
+// int y = int.Parse(Console.ReadLine());
+// int z = int.Parse(Console.ReadLine());
+// int max = x;
+//  if (max<y) max = y;
+//  if (max<z) max = z;
+// Console.WriteLine(" - > " + max);
+
+// Задача 6: Напишите программу, которая на вход принимает число 
+// и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.WriteLine("Напишите любое число :");
+// int nam = int.Parse(Console.ReadLine()!) ;
+//    if (nam % 2 == 0)
+//    {
+//     Console.WriteLine(nam + " -> Да, Число четное");
+//    }
+//    else
+//    {
+//     Console.WriteLine(nam + " -> Нет, Число нечетное");
+//    }
+
+//   Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+ 
+
