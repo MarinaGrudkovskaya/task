f570ec55797246c1496eeee5442435a9bd9f5d8f
MarinaGrudkovskaya <mgg1005@yandex.ru>
2023-07-10 23:19:29 +0300

    сделала домашку по второму семинару

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,5 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+﻿// Домашка по второму семинару:
+
 // Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
 // и на выходе показывает вторую цифру этого числа.
 // 456 -> 5
@@ -18,15 +18,42 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-Console.WriteLine("Введите число:");
-int a = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите число:");
+// int a = int.Parse(Console.ReadLine()!);
+//    if (a<100)
+//    {
+//     Console.WriteLine(a + "->третьей цифры нет");
+//    }
+//    if (a>99 && a<1000)
+//    {
+//     int a1 = a%10;
+//     Console.WriteLine(a + "-> " + a1);
+//    }
+// if (a>999 && a<10000)
+//    {
+//     int a2 = (a%100-a%10)/10;
+//     Console.WriteLine(a + "-> " + a2);
+//    }
+// if (a>9999 && a<100000)
+//    {
+//     int a3 = (a%1000-a%100)/100;
+//     Console.WriteLine(a + "-> " + a3);
+//    }
 
-if (a>=10 & a<100)
- {
-    Console.WriteLine("Третьей цифры нет(");
- }
-if (a>=100)
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Ввудите цифру от 1 до 7:");
+int d = int.Parse(Console.ReadLine()!);
+if (d<=5)
 {
-  int nam=(a%100-a%1000)/100;
-    Console.WriteLine(-nam);
+
+  Console.WriteLine(d + " -> нет");
+}
+else
+{
+   Console.WriteLine(d + " -> да");
 }
