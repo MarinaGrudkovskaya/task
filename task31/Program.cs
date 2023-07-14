f8089eb0f813f8326571ab59494a20c05cf53f7d
MarinaGrudkovskaya <mgg1005@yandex.ru>
2023-07-14 09:43:25 +0300

    Домашка 3, задача 1, не получается вывод

--- task31/Program.cs
+++ task31/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int CallNumber(string message)  
+{
+Console.WriteLine(message);
+int number = int.Parse(Console.ReadLine());
+return number;
+}
+
+int num = CallNumber("Введите пятизначное число: "); 
+
+
+
+if ( (( num/1000)-(num%100)) == 0);
+
+
+ Console.WriteLine(num + " -> да");
+ 
+
+if ( (( num/1000)-(num%100)) != 0);
+
+    
+ Console.WriteLine(num + " -> нет");
+    
+
+
 
