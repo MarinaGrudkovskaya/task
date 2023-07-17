e25916822dda0c54751ef427b29f7e497f0aa6ea
MarinaGrudkovskaya <mgg1005@yandex.ru>
2023-07-17 08:23:00 +0300

    дз3

---
+++ Seminar/task2/Program.cs
@@ -0,0 +1,24 @@
+﻿
+// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int PointCoordinat(string  message)     
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int x1 = PointCoordinat("Введите точку x1: ");
+int y1 = PointCoordinat("Введите точку y1: ");
+int z1 = PointCoordinat("Введите точку z1: ");
+int x2 = PointCoordinat("Введите точку x2: ");
+int y2 = PointCoordinat("Введите точку y2: ");
+int z2 = PointCoordinat("Введите точку z2: ");
+
+
+double xyz = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+ Console.WriteLine(Math.Round(xyz, 2));
---
+++ Seminar/task3/Program.cs
@@ -0,0 +1,32 @@
+﻿
+// // Задача 23
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int Prompt(string message)
+{
+Console.Clear();
+Console.Write(message);
+int number = int.Parse(Console.ReadLine());
+return number;
+}
+int num = Prompt("Введите чиcло:  ");
+void CubeNumber(int num)
+{
+    int count = 1; 
+    while (count <= num)
+    {
+        if (count == num)
+        {
+          Console.WriteLine(count*count*count);   
+        }
+        else
+        Console.Write(count*count*count + "," );
+     count ++;
+    }
+    
+}
+Console.Write($"{num} -> ");
+ CubeNumber(num);
