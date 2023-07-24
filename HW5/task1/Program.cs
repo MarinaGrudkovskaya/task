740b49f860910ee777950ad9ba3deea69f88ab9b
MarinaGrudkovskaya <mgg1005@yandex.ru>
2023-07-24 21:10:27 +0300

    Домашняя работа по 5 семинару

---
+++ HW5/task1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] GetArray(int size, int minValue, int MaxValue)  //создаем массив
+{
+int[] result = new int[size];
+for (int i = 0; i < size; i++)
+{
+    result[i] = new Random().Next(100, 1000);
+    Console.Write($"{result[i]}  ");
+}
+    return result;
+}
+int[] array = GetArray(10, 100, 1000);
+
+
+
+ int count = 0;
+for (int i = 0; i < 10; i++)
+{
+  if (array[i]%2==0)
+{
+count=count+1;
+}
+}
+  Console.WriteLine();
+  Console.WriteLine($" количество чётных чисел в массиве: {count}"); 
+
+
+ 
+
+
+  
+
+
---
+++ HW5/task3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+int GetNams(string massege)
+{
+  Console.WriteLine(massege);
+  int num = int.Parse(Console.ReadLine());
+   return num;
+}
+int sizeAr =GetNams("Введите длинну: ");
+int minAr =GetNams("Введите минимальное число: ");
+int maxAr =GetNams("Введите максимальное число: ");
+double[] ar = new double[sizeAr];
+for (int i = 0; i < sizeAr; i++)
+{
+     ar[i] = new Random().Next(minAr, maxAr);
+     Console.Write($"{ar[i]}  ");
+}
+double min = ar[0];
+double max = ar[0];
+
+for (int i = 0; i < sizeAr; i++)
+{
+    if (ar[0] >= ar[i])
+    min = ar[i];
+}
+for (int i = 0; i < sizeAr; i++)
+{
+     if (ar[0]<=ar[i])
+    max = ar[i];
+
+}
+Console.WriteLine();
+Console.WriteLine($"{max-min}");
