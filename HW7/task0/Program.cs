adc569b5d8a04b88b4eb1a8e92889cd4958f9b23
MarinaGrudkovskaya <mgg1005@yandex.ru>
2023-07-31 22:09:41 +0300

    HW7

---
+++ HW7/task0/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+int rows = GetNumber("введите колличество строк: ");
+int columns = GetNumber("введите колличество столбцов: ");
+double[,] array = GetArray(rows, columns);
+
+int GetNumber(string messege)
+{
+    Console.Write(messege);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+double[,] GetArray(int row, int column)
+{
+double[,] res = new double[row, column];
+for (int i = 0; i < row; i++)
+{
+    for (int j = 0; j < column; j++)
+    {
+        res[i,j] = new Random().NextDouble()*10-5;
+        res[i,j] = Math.Round(res[i,j], 1); 
+        Console.Write($"{res[i, j]}  | ");
+    }
+    Console.WriteLine();
+}   
+return res;
+}
+//  Console.WriteLine();
+// Console.Write(array);
---
+++ HW7/task3/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+Console.Clear();
+int row = GetNumber("Введите количество строк: ");
+int column = GetNumber("Введите количество столбцов: ");
+int[,] array = GetArray(row,column);
+GetSumArray(array);
+
+Console.Write(String.Join(" ", array ));
+
+int GetNumber(string messege)
+{
+Console.WriteLine(messege);
+return int.Parse(Console.ReadLine());
+}
+
+int[,] GetArray(int row, int column)
+{
+ int[,] result = new int[row, column];
+ for (int i = 0; i < row; i++)
+ {
+    for (int j = 0; j < column; j++)
+    {
+        result[i,j] = new Random().Next(1,10);
+         Console.Write($"{result[i, j]}  ");
+    }
+     Console.WriteLine();
+ }
+return result;
+}
+
+void GetSumArray(int[,] array)
+{
+double sumMedium = 0;
+double[] sum = new double[row];   
+ for (int i = 0; i < row; i++)
+ {
+    for (int j = 0; j < column; j++)
+    {
+        sumMedium = sumMedium + array[i,j];
+        sum[i] = sumMedium/column;
+     sum[i] = Math.Round(sum[i], 3);  
+    
+    }
+     sumMedium =0;
+    
+    Console.Write($" {sum[i]}  ");
+ }
+}
+            
